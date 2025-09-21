2b9b1bb1647f3e6cc5675c14be44860e33959bc7
Danish Khan <dk831675@gmail.com>
2025-09-21 16:10:33 +0530

    build fixed #5...

--- src/components/sections/Home/CreatorsSpotlightWall.tsx
+++ src/components/sections/Home/CreatorsSpotlightWall.tsx
@@ -5,10 +5,10 @@ const CreatorsSpotlightWall = () => {
   return (
     <section className='max-w-[1500px] w-full mx-auto relative px-4 sm:px-6 md:px-8 flex flex-col justify-start items-start mt-[110px] sm:mt-[150px] md:mt-[170px] lg:mt-[190px]'>
       
-        <h2 className="text-[28px] sm:text-[40px] md:text-[45px] lg:text-[50px] font-semibold capitalize"> Creator's Spotlight Wall </h2>
+        <h2 className="text-[28px] sm:text-[40px] md:text-[45px] lg:text-[50px] font-semibold capitalize"> Creator&apos;s Spotlight Wall </h2>
 
         <p className='text-start max-w-[1180px] w-full font-normal text-[#2C2C2C] text-[14px] sm:text-[15px] md:text-base lg:text-[17.5px] leading-tight mt-[18px] sm:mt-[20px] md:mt-[23px] lg:mt-[25px]'>
-          Celebrating the talent, passion, and creativity of our community. From filmmakers to photographers, dancers to digital creators — here’s where we showcase the incredible work made inside Rent for Reel spaces.
+          Celebrating the talent, passion, and creativity of our community. From filmmakers to photographers, dancers to digital creators — here&apos;s where we showcase the incredible work made inside Rent for Reel spaces.
         </p>
 
 
